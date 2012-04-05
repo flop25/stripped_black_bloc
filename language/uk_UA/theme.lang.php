005dd8611ac9fc4420c65c25d5e4c102b28d7859
flop25 <flop25@piwigo.org>
2012-04-05 16:54:42 +0000

    uk_UA update thx to animan
    git-svn-id: http://piwigo.org/svn/extensions/stripped_black_bloc@13922 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/uk_UA/theme.lang.php
+++ language/uk_UA/theme.lang.php
@@ -1,17 +1,17 @@
 <?php
-$lang['The parent-theme Stripped need to be enabled in order to use its parameters'] = 'The parent-theme Stripped need to be enabled in order to use its parameters';
+$lang['The parent-theme Stripped need to be enabled in order to use its parameters'] = 'Батьківська тема Stripped потрібна для активації її параметрів';
 $lang['Stripped & Columns Theme Configuration'] = 'Stripped & Columns, Сторінка конфігурації';
 $lang['Page'] = 'Сторінка';
-$lang['color main'] = "Не сірим кольором :";
+$lang['color main'] = "Основний колір :";
 $lang['Displaying options'] = "Показати варіанти";
 $lang['Width of columns'] = "Ширина стовпців";
 $lang['Width of thumbnails'] = "Ширина мініатюри";
-$lang['Create a big thumbnail every'] = "Create a big thumbnail every";
-$lang['... starting to the position number'] = "... starting to the position number";
-$lang['(set 0 to disable)'] = "(set 0 to disable)";
-$lang['(it need to be inferior than the width of columns)'] = "(вона повинна суворо поступаютися ширину стовпців)";
-$lang['About big thumbnails, for the albums :'] = "Мініатюри можуть бути отримані на тему";
-$lang['don\'t generate big thumbnails'] = "don't generate big thumbnails";
-$lang['use the same config as above'] = "use the same config as above";
-$lang['always generate big thumbnails'] = "always generate big thumbnails";
+$lang['Create a big thumbnail every'] = "Створювати великі мініатюри завжди";
+$lang['... starting to the position number'] = "... починаючи з позиції номер";
+$lang['(set 0 to disable)'] = "(встановіть 0 для відключення)";
+$lang['(it need to be inferior than the width of columns)'] = "(вона повинна суворо поступаютися ширині стовпців)";
+$lang['About big thumbnails, for the albums :'] = "Про великі мініатюри для альбомів :";
+$lang['don\'t generate big thumbnails'] = "не створювати великі мініатюри";
+$lang['use the same config as above'] = "використовуйте ті ж конфігурації, що і вище";
+$lang['always generate big thumbnails'] = "завжди створювати великі мініарюри";
 ?>
