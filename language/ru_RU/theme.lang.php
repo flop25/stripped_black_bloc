b16b38ac1e8e9cf7f50b9488ae297e76d0ac68f9
TranslationTeam <translate-github@piwigo.org>
2012-04-10 04:24:56 +0000

    [Stripped & Columns] Add ru_RU, thanks to : yuragal
    git-svn-id: http://piwigo.org/svn/extensions/stripped_black_bloc@14061 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/ru_RU/theme.lang.php
@@ -0,0 +1,38 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['(it need to be inferior than the width of columns)'] = '(значение должно быть меньше, чем ширина колонок)';
+$lang['(set 0 to disable)'] = '(установите значение 0 для отключения)';
+$lang['... starting to the position number'] = '... начиная с позиции';
+$lang['About big thumbnails, for the albums :'] = 'Настройки больших миниатюр альбомов:';
+$lang['Create a big thumbnail every'] = 'Создавать большие миниатюры каждые';
+$lang['Displaying options'] = 'Настройки отображения';
+$lang['Page'] = 'Страница';
+$lang['Stripped & Columns Theme Configuration'] = 'Stripped & Columns, страница настроек';
+$lang['The parent-theme Stripped need to be enabled in order to use its parameters'] = 'Основная тема \'Stripped\' должна быть включена для того, чтобы получить возможность использовать ее параметры';
+$lang['Width of columns'] = 'Ширина колонок';
+$lang['Width of thumbnails'] = 'Ширина миниатюр';
+$lang['always generate big thumbnails'] = 'Всегда создавать большие миниатюры';
+$lang['color main'] = 'Не серый цвет';
+$lang['don\'t generate big thumbnails'] = 'Не создавать большие миниатюры';
+$lang['use the same config as above'] = 'Использовать настройки, определенные для миниатюр фотографий';
+?>
