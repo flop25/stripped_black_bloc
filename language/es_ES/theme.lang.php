228b74c645cf12909077adc8065409c484b5c90c
ddtddt <ddtddt@piwigo.org>
2012-04-14 05:31:14 +0000

    [extensions] - stripped_black_blo - add es_ES - Español – Spanish - Thanks jpr928
    git-svn-id: http://piwigo.org/svn/extensions/stripped_black_bloc@14150 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/es_ES/theme.lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['The parent-theme Stripped need to be enabled in order to use its parameters'] = 'El tema-padre "Stripped" debe estar activo para poder utilisar sus parametros.';
+$lang['Stripped & Columns Theme Configuration'] = 'Stripped & Columns, Pagina de configuración';
+$lang['Page'] = 'Pagina';
+$lang['color main'] = "Color no gris :";
+$lang['Displaying options'] = "Optiones de visualisación";
+$lang['Width of columns'] = "Ancho de las columnas";
+$lang['Width of thumbnails'] = "Ancho de las miniaturas";
+$lang['Create a big thumbnail every'] = "Crear una gran miniatura cada";
+$lang['... starting to the position number'] = "...Empezando por la posición numero";
+$lang['(set 0 to disable)'] = "(poner 0 para desactivar)";
+$lang['(it need to be inferior than the width of columns)'] = "(este ancho debe ser estrictamente inferior a la de las columnas)";
+$lang['About big thumbnails, for the albums :'] = "A proposito de grandes miniaturas, para las imagenes representativas de los albumes :";
+$lang['don\'t generate big thumbnails'] = "no generar grandes miniaturas";
+$lang['use the same config as above'] = 'utilisar la configuración "Crear una grande miniatura cada" aqui ariba';
+$lang['always generate big thumbnails'] = "Siempre generar grandes miniaturas";
+?>
