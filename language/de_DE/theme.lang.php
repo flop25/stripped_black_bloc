216ae64be43e8a69e0c699a1ab6c03192f9fdfc0
TranslationTeam <translate-github@piwigo.org>
2012-04-21 23:20:08 +0000

    [Stripped & Columns] Add Deutsch [DE], thanks to : Joergen
    git-svn-id: http://piwigo.org/svn/extensions/stripped_black_bloc@14253 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/de_DE/theme.lang.php
@@ -0,0 +1,38 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['(it need to be inferior than the width of columns)'] = '(es muss strikt kleiner, als die Breite der Spalten)';
+$lang['(set 0 to disable)'] = '(wähle o zum ausschalten)';
+$lang['... starting to the position number'] = 'Starte an der Positions-Nummer';
+$lang['About big thumbnails, for the albums :'] = 'Über große Vorschaubilder, für die Alben:';
+$lang['Create a big thumbnail every'] = 'Erstelle immer eine große Vorschaubilder';
+$lang['Displaying options'] = 'Anzeigeoptionen';
+$lang['Page'] = 'Seite';
+$lang['Stripped & Columns Theme Configuration'] = 'Stripped & Columns, Konfigurationsseite';
+$lang['The parent-theme Stripped need to be enabled in order to use its parameters'] = 'Um die Parameter zu benutzen muss das Eltern Design Stripped aktiviert sein';
+$lang['Width of columns'] = 'Breite von Spalten';
+$lang['Width of thumbnails'] = 'Breite der Vorschaubilder';
+$lang['always generate big thumbnails'] = 'Immer große Vorschaubilder erzeugen';
+$lang['color main'] = 'Nicht die graue Farbe';
+$lang['don\'t generate big thumbnails'] = 'erzeugen keine großen Vorschaubilder';
+$lang['use the same config as above'] = 'verwende die gleiche Konfiguration wie oben';
+?>
