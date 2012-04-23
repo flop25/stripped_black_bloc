7b3904e77c4eec7af7d468fd9fa378ed02c22a4b
TranslationTeam <translate-github@piwigo.org>
2012-04-23 06:08:32 +0000

    [stripped_columns] Add sk_SK, thanks to : dodo
    git-svn-id: http://piwigo.org/svn/extensions/stripped_black_bloc@14271 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/sk_SK/theme.lang.php
@@ -0,0 +1,38 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['(it need to be inferior than the width of columns)'] = '(potreba presného nastavenia horšej kvality ako je šírka stĺpcov)';
+$lang['(set 0 to disable)'] = '(nastaviť 0 pre zakázanie)';
+$lang['... starting to the position number'] = '...spúšťanie na pozícii číslo';
+$lang['About big thumbnails, for the albums :'] = 'O veľkých náhľadoch for albumy:';
+$lang['Create a big thumbnail every'] = 'Vytvoriť všetky veľké náhľady';
+$lang['Displaying options'] = 'Voľba zobrazení';
+$lang['Page'] = 'Stránka';
+$lang['Stripped & Columns Theme Configuration'] = 'Zdroj & Stĺpce, konfiguračná stránka';
+$lang['The parent-theme Stripped need to be enabled in order to use its parameters'] = 'Zdroj témy potrebuje byť povolený pri použití zoraďovacích parametrov';
+$lang['Width of columns'] = 'Šírka stĺpcov';
+$lang['Width of thumbnails'] = 'Šírka náhľadov';
+$lang['always generate big thumbnails'] = 'vždy generovať veľké náhľady';
+$lang['color main'] = 'Bez šedej farby:';
+$lang['don\'t generate big thumbnails'] = 'negenerovať veľké náhľady';
+$lang['use the same config as above'] = 'použiť rovnakú konfiguráciu ako hore';
+?>
