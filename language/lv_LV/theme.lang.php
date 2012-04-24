7bb5aaf2793cc04f5db6d0d2137cbb0bf11ca879
ddtddt <ddtddt@piwigo.org>
2012-04-24 06:58:45 +0000

    [extensions] - stripped_black_bloc - add lv_LV - Latviešu - Latvian - Thanks to Aivars Baldone
    git-svn-id: http://piwigo.org/svn/extensions/stripped_black_bloc@14320 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/lv_LV/theme.lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['Stripped & Columns Theme Configuration'] = 'Atvieglots & Kolonas, Konfigurācijas Lapa';
+$lang['Page'] = 'Lapa';
+$lang['color main'] = "Ne pelēka krāsa :";
+$lang['Displaying options'] = "Displeja opcijas";
+$lang['Width of columns'] = "Kolonu platums";
+$lang['Width of thumbnails'] = "Sīkbilžu platums";
+$lang['The parent-theme Stripped need to be enabled in order to use its parameters'] = 'Atvieglotā pamattēma ir jāiespējo, lai lietotu tās parametrus';
+$lang['Create a big thumbnail every'] = 'Izveidot lielu katru sīkbildi';
+$lang['... starting to the position number'] = '... sākot ar pozīcijas numuru';
+$lang['(set 0 to disable)'] = '(iestatīt 0 kā atspējotu)';
+$lang['(it need to be inferior than the width of columns)'] = '(tam būtu jābūt strikti atkarīgam no kolonu platuma)';
+$lang['About big thumbnails, for the albums :'] = 'Par albūmu lielajām sīkbildēm :';
+$lang['don\'t generate big thumbnails'] = 'neveidot lielās sīkbildes';
+$lang['use the same config as above'] = 'lietot to pašu konfigurāciju kā iepriekš';
+$lang['always generate big thumbnails'] = 'vienmēr veidot lielās sīkbildes';
+?>
