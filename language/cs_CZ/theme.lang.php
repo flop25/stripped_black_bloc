d6eb352d6b55d65d82c2667dc8d674ff0eebd597
TranslationTeam <translate-github@piwigo.org>
2012-04-24 14:24:37 +0000

    [stripped_columns] Add cs_CZ, thanks to : sichr
    git-svn-id: http://piwigo.org/svn/extensions/stripped_black_bloc@14345 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/cs_CZ/theme.lang.php
@@ -0,0 +1,31 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Displaying options'] = 'Nastavení zobrazení';
+$lang['Page'] = 'Strana';
+$lang['Width of columns'] = 'Šířka sloupců';
+$lang['Width of thumbnails'] = 'Šířka náhledů';
+$lang['always generate big thumbnails'] = 'vždy generovat velké náhledy';
+$lang['color main'] = 'Barva jiná než šedá:';
+$lang['don\'t generate big thumbnails'] = 'negenerovat velké náhledy';
+$lang['use the same config as above'] = 'použít stejné nastavení jako výše';
+?>
