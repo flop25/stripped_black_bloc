0bce82a1b2f825b34fb238908f2ff581b8352395
TranslationTeam <translate-github@piwigo.org>
2012-04-27 21:51:23 +0000

    [stripped_columns] Add hu_HU, thanks to : samli
    git-svn-id: http://piwigo.org/svn/extensions/stripped_black_bloc@14491 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/hu_HU/theme.lang.php
@@ -0,0 +1,38 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['(it need to be inferior than the width of columns)'] = '(kötelezően kisebb mint az oszlopok szélessége)';
+$lang['(set 0 to disable)'] = '(tiltáshoz állítsa 0-ra)';
+$lang['... starting to the position number'] = '... kezdő pozíció szám';
+$lang['About big thumbnails, for the albums :'] = 'Nagy bélyegképek az albumokhoz :';
+$lang['Create a big thumbnail every'] = 'Nagy bélyegképek létrehozása';
+$lang['Displaying options'] = 'Kinézet beállítása';
+$lang['Page'] = 'Oldal';
+$lang['Stripped & Columns Theme Configuration'] = 'Stripped & Columns beállító oldal';
+$lang['The parent-theme Stripped need to be enabled in order to use its parameters'] = 'A Stripped szülő-témát be kell kapcsolni a beállítások aktiválásához';
+$lang['Width of columns'] = 'Oszlopszélesség';
+$lang['Width of thumbnails'] = 'Bélyegképek szélessége';
+$lang['always generate big thumbnails'] = 'mindig készüljenek nagy bélyegképek';
+$lang['color main'] = 'Nem szürke színű:';
+$lang['don\'t generate big thumbnails'] = 'ne készüljenek nagy bélyegképek';
+$lang['use the same config as above'] = 'fenti beállítások használata';
+?>
