55df4a2568d0c2a494f2ca459601030cfc64273d
TranslationTeam <translate-github@piwigo.org>
2012-06-23 09:46:27 +0000

    [stripped_columns] Update tr_TR, thanks to : LazBoy
    git-svn-id: http://piwigo.org/svn/extensions/stripped_black_bloc@15965 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/tr_TR/theme.lang.php
@@ -0,0 +1,38 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['(it need to be inferior than the width of columns)'] = '(kolon genişliğinden daha aşağıda olması gereklidir)';
+$lang['(set 0 to disable)'] = '(pasifleştirmek için 0 seçin)';
+$lang['... starting to the position number'] = '...pozisyon numarası ile başlıyor';
+$lang['About big thumbnails, for the albums :'] = 'Albümler için büyük pul resimleri hakkında:';
+$lang['Create a big thumbnail every'] = 'Büyük bir pul resmi yarat her';
+$lang['Displaying options'] = 'Görüntüleme seçenekleri';
+$lang['Page'] = 'Sayfa';
+$lang['Stripped & Columns Theme Configuration'] = 'Stripped & Columns, Yapılandırma Sayfası';
+$lang['The parent-theme Stripped need to be enabled in order to use its parameters'] = 'Parametreleri kullanabilmek için ana tema olan Stipped etkinleştirilmelidir';
+$lang['Width of columns'] = 'Sütun genişliği';
+$lang['Width of thumbnails'] = 'Pul resmi (thumbnail) genişliği';
+$lang['always generate big thumbnails'] = 'her zaman büyük pul resimleri yarat';
+$lang['color main'] = 'Gri renk değil:';
+$lang['don\'t generate big thumbnails'] = 'büyük pul resimleri yaratma';
+$lang['use the same config as above'] = 'yukarıdaki konfigürasyonu kullan';
+?>
