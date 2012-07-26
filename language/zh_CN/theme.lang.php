7c1863f924babb8939cb6b6c2b553af5620543c3
TranslationTeam <translate-github@piwigo.org>
2012-07-26 10:48:28 +0000

    [stripped_columns] Add zh_CN, thanks to : deserteagle
    git-svn-id: http://piwigo.org/svn/extensions/stripped_black_bloc@17131 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/zh_CN/theme.lang.php
@@ -0,0 +1,38 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['(it need to be inferior than the width of columns)'] = '（必须严格小于列宽）';
+$lang['(set 0 to disable)'] = '（设为禁止）';
+$lang['... starting to the position number'] = '...开始位置号码';
+$lang['About big thumbnails, for the albums :'] = '关于相册的大缩略图：';
+$lang['Create a big thumbnail every'] = '每次创建大缩略图';
+$lang['Displaying options'] = '显示选项';
+$lang['Page'] = '页';
+$lang['Stripped & Columns Theme Configuration'] = '分离及列配置页面';
+$lang['The parent-theme Stripped need to be enabled in order to use its parameters'] = '此参数需开启父主题分离';
+$lang['Width of columns'] = '列宽';
+$lang['Width of thumbnails'] = '缩略图宽';
+$lang['always generate big thumbnails'] = '总是生成大缩略图';
+$lang['color main'] = '非灰度';
+$lang['don\'t generate big thumbnails'] = '不生成大缩略图';
+$lang['use the same config as above'] = '使用以上相同配置';
+?>
