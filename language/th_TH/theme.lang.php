4a17468e6c0110bbd3ea3908c4e035fc256c56c7
TranslationTeam <translate-github@piwigo.org>
2012-09-11 18:42:39 +0000

    [stripped_columns] Update th_TH, thanks to : pradit
    git-svn-id: http://piwigo.org/svn/extensions/stripped_black_bloc@17865 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/th_TH/theme.lang.php
@@ -0,0 +1,30 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['color main'] = 'ไม่มีสีเทา';
+$lang['don\'t generate big thumbnails'] = 'ไม่ต้องสร้างภาพขนาดเล็ก';
+$lang['use the same config as above'] = 'ใช้การตั้งค่าเดียวกันกับข้างต้น';
+$lang['Width of columns'] = 'ความกว้างของคอลัมน์';
+$lang['Width of thumbnails'] = 'ความกว้างของภาพขนาดเล็ก';
+$lang['Displaying options'] = 'ตัวเลือกการแสดง';
+$lang['Page'] = 'หน้า';
+?>
