1e4b026c8d604c91f03b79099d5b9cda4e75c793
flop25 <flop25@piwigo.org>
2012-09-12 14:26:32 +0000

    En improved thx to JFF
    git-svn-id: http://piwigo.org/svn/extensions/stripped_black_bloc@17872 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/en_UK/theme.lang.php
+++ language/en_UK/theme.lang.php
@@ -1,5 +1,5 @@
 <?php
-$lang['The parent-theme Stripped need to be enabled in order to use its parameters'] = 'The parent-theme Stripped need to be enabled in order to use its parameters';
+$lang['The parent-theme Stripped need to be enabled in order to use its parameters'] = 'The parent-theme Stripped must be enabled in order to use its parameters';
 $lang['Stripped & Columns Theme Configuration'] = 'Stripped & Columns, Configuration Page';
 $lang['Page'] = 'Page';
 $lang['color main'] = "Not grey color :";
@@ -9,8 +9,8 @@ $lang['Width of thumbnails'] = "Width of thumbnails";
 $lang['Create a big thumbnail every'] = "Create a big thumbnail every";
 $lang['... starting to the position number'] = "... starting to the position number";
 $lang['(set 0 to disable)'] = "(set 0 to disable)";
-$lang['(it need to be inferior than the width of columns)'] = "(it needs to be strictly inferior than the width of columns)";
-$lang['About portrait/landscape pictures :'] = "About portrait/landscape pictures (Note that the plugin auto-manage the thumbnails of panorama):";
+$lang['(it need to be inferior than the width of columns)'] = "(it needs to be strictly smaller than the width of columns)";
+$lang['About portrait/landscape pictures :'] = "About portrait/landscape pictures (Note that the plugin auto-manages the thumbnails of panoramas):";
 $lang['No Maximum Height and the width is the one set above'] = "No Maximum Height and the width is the one set above";
 $lang['Use the width parameter above as a maximum heigth'] = "Use the width parameter above as a maximum heigth";
 $lang['Display big thumbnails for landscape pictures'] = "Display big thumbnails for landscape pictures";
