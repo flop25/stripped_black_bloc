f64f7bbe69694e1e59b7a825e9e92303b9ea05d5
TranslationTeam <translate-github@piwigo.org>
2012-09-14 15:08:53 +0000

    [stripped_columns] Update da_DK, thanks to : thomas1956
    git-svn-id: http://piwigo.org/svn/extensions/stripped_black_bloc@17902 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/da_DK/theme.lang.php
@@ -0,0 +1,42 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['(it need to be inferior than the width of columns)'] = '(skal altid være mindre end kolonnebredden)';
+$lang['(set 0 to disable)'] = '(angiv "0" for at deaktivere)';
+$lang['... starting to the position number'] = '...start til position nummer';
+$lang['About big thumbnails, for the albums :'] = 'Store thumbnails, for albums:';
+$lang['About portrait/landscape pictures :'] = 'Billeder i portræt-/landskabsformat (bemærk at plugin\'en automatisk håndterer thumbnails for panoramaer)';
+$lang['Create a big thumbnail every'] = 'Generer en stor thumbnail hver';
+$lang['Display big thumbnails for landscape pictures'] = 'Vis store thumbnails for billeder i landskabsformat';
+$lang['Displaying options'] = 'Indstillinger for visning';
+$lang['No Maximum Height and the width is the one set above'] = 'Ingen maksimum højde. Bredde er som indstillet ovenfor';
+$lang['Page'] = 'Side';
+$lang['Stripped & Columns Theme Configuration'] = 'Stripped & Columns, ';
+$lang['The parent-theme Stripped need to be enabled in order to use its parameters'] = 'Master temaet Stripped skal være aktiveret for at kunne benytte indstillingerne for dette tema';
+$lang['Use the width parameter above as a maximum heigth'] = 'Brug indstillingen for bredde ovenfor som maksimum højde ';
+$lang['Width of columns'] = 'Bredde på kolonner';
+$lang['Width of thumbnails'] = 'Bredde på thumbnails';
+$lang['always generate big thumbnails'] = 'generer altid thumbnails';
+$lang['color main'] = 'Ikke grå farve:';
+$lang['don\'t generate big thumbnails'] = 'generer ikke store thumbnails';
+$lang['use the same config as above'] = 'brug samme indstillinger som ovenfor';
+?>
