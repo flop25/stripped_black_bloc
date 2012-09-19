8d110d2fe664a6634e146f4bfe9a8540d84677c0
TranslationTeam <translate-github@piwigo.org>
2012-09-19 12:25:36 +0000

    [stripped_columns] Update pt_BR, thanks to : oigreslima
    git-svn-id: http://piwigo.org/svn/extensions/stripped_black_bloc@18048 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/pt_BR/theme.lang.php
@@ -0,0 +1,42 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['(it need to be inferior than the width of columns)'] = 'Precisa ser menor do que a largura das colunas';
+$lang['(set 0 to disable)'] = 'Use 0 para desabilitar';
+$lang['... starting to the position number'] = '...inicia na posição número';
+$lang['About big thumbnails, for the albums :'] = 'Sobre grandes miniaturas, para os álbuns';
+$lang['About portrait/landscape pictures :'] = 'Sobre fotos em Portrait/Landscape (Note que o plugin auto-gerencia as miniaturas de panoramas):';
+$lang['Create a big thumbnail every'] = 'Cria uma grande miniatura  a cada';
+$lang['Display big thumbnails for landscape pictures'] = 'Mostre grandes miniaturas para fotos em landscape';
+$lang['Displaying options'] = 'Opções de Aparência';
+$lang['No Maximum Height and the width is the one set above'] = 'Sem altura máxima e a largura é a definida acima';
+$lang['Page'] = 'Página';
+$lang['Stripped & Columns Theme Configuration'] = 'Página de configuração do Stripped & Columns';
+$lang['The parent-theme Stripped need to be enabled in order to use its parameters'] = 'O tema pai Stripped deve ser habilitado para usar seus parâmetros';
+$lang['Use the width parameter above as a maximum heigth'] = 'Use a largura acima como altura máxima';
+$lang['Width of columns'] = 'Largura das colunas';
+$lang['Width of thumbnails'] = 'Largura das miniaturas';
+$lang['always generate big thumbnails'] = 'Sempre gere grandes miniaturas';
+$lang['color main'] = 'Sem cor cinza:';
+$lang['don\'t generate big thumbnails'] = 'Não gere grandes miniaturas';
+$lang['use the same config as above'] = 'Use  a mesma configuração acima';
+?>
