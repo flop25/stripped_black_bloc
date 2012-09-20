3e0f6974b5006e0d3120fb2c40a3c095015e606a
TranslationTeam <translate-github@piwigo.org>
2012-09-20 16:02:59 +0000

    [stripped_columns] Update nl_NL, thanks to : Kees Hessels
    git-svn-id: http://piwigo.org/svn/extensions/stripped_black_bloc@18075 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/nl_NL/theme.lang.php
+++ language/nl_NL/theme.lang.php
@@ -20,19 +20,23 @@
 // | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
 // | USA.                                                                  |
 // +-----------------------------------------------------------------------+
-$lang['(it need to be inferior than the width of columns)'] = '(Het moet kleiner zijn dan de breedte van de kolommen)';
+$lang['(it need to be inferior than the width of columns)'] = '(het moet kleiner zijn dan de breedte van de kolommen)';
 $lang['(set 0 to disable)'] = '(zet op 0 om uit te schakelen)';
-$lang['... starting to the position number'] = '...';
-$lang['About big thumbnails, for the albums :'] = 'Over grote minaturen, voor de albums';
-$lang['Create a big thumbnail every'] = 'genereer een grote miniatuur elke';
+$lang['... starting to the position number'] = '...beginnend bij positie nummer';
+$lang['About big thumbnails, for the albums :'] = 'Over grote miniaturen, voor de albums:';
+$lang['Create a big thumbnail every'] = 'Genereer een grote miniatuur elke';
 $lang['Displaying options'] = 'Weergave opties';
 $lang['Page'] = 'Pagina';
-$lang['Stripped & Columns Theme Configuration'] = 'Stripped & Columns, configuratie pagina';
-$lang['The parent-theme Stripped need to be enabled in order to use its parameters'] = 'Het hoofd thema Stripped moet actief zijn, om  zijn instellingen te kunnen gebruiken';
+$lang['Stripped & Columns Theme Configuration'] = 'Stripped & Columns, Instellingen-pagina';
+$lang['The parent-theme Stripped need to be enabled in order to use its parameters'] = 'Het hoofd thema Stripped moet actief zijn, om de instellingen van dit thema te kunnen gebruiken';
 $lang['Width of columns'] = 'Breedte van kolommen';
-$lang['Width of thumbnails'] = 'Breedte van minituren';
+$lang['Width of thumbnails'] = 'Breedte van miniaturen';
 $lang['always generate big thumbnails'] = 'genereer altijd grote miniaturen';
 $lang['color main'] = 'Niet grijze kleur:';
 $lang['don\'t generate big thumbnails'] = 'genereer geen grote miniaturen';
-$lang['use the same config as above'] = 'gebruik dezelfde configuratie als boven';
+$lang['use the same config as above'] = 'gebruik dezelfde instellingen als boven';
+$lang['No Maximum Height and the width is the one set above'] = 'Geen maximale hoogte en de breedte is zoals hierboven aangegeven';
+$lang['Display big thumbnails for landscape pictures'] = 'Gebruik grote miniaturen voor landschap-afbeeldingen';
+$lang['About portrait/landscape pictures :'] = 'Over portret/landschap foto\'s (Hou er rekening mee dat deze plugin de miniaturen van panorama\'s automatisch beheert):';
+$lang['Use the width parameter above as a maximum heigth'] = 'Gebruik de breedte-parameter hierboven als maximum hoogte';
 ?>
