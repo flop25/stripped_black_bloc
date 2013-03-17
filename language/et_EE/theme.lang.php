657fdacfbcc50cdbdbcf0cca05739be22154644b
TranslationTeam <translate-github@piwigo.org>
2013-03-17 08:58:20 +0000

    [stripped_columns] Add et_EE, thanks to : Eggy
    git-svn-id: http://piwigo.org/svn/extensions/stripped_black_bloc@21625 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/et_EE/theme.lang.php
@@ -0,0 +1,39 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Width of columns'] = 'Veergude laius';
+$lang['Width of thumbnails'] = 'Kuvapiltide laius';
+$lang['always generate big thumbnails'] = 'alati genereeri suured kuvapildid';
+$lang['color main'] = 'Mitte hall värv:';
+$lang['don\'t generate big thumbnails'] = 'ära genereeri suuri kuvapilte';
+$lang['use the same config as above'] = 'kasuta sama seadistust nagu ülal';
+$lang['Use the width parameter above as a maximum heigth'] = 'Kasuta laiusparameetrit maksimaalse kõrgusena';
+$lang['The parent-theme Stripped need to be enabled in order to use its parameters'] = 'Peateema Stripped tuleb aktiveerida, et seda parameetrit kasutada';
+$lang['Displaying options'] = 'Kuva valikud';
+$lang['Display big thumbnails for landscape pictures'] = 'Kuva suur kuvapilt horisontaalpaigutusega piltidele';
+$lang['Page'] = 'Leht';
+$lang['Create a big thumbnail every'] = 'Loo suur kuvapilt iga';
+$lang['About big thumbnails, for the albums :'] = 'Suurtest kuvapiltides albumite jaoks:';
+$lang['(set 0 to disable)'] = '(deaktiveeri)';
+$lang['... starting to the position number'] = '...alustades positsiooni numbrist';
+$lang['(it need to be inferior than the width of columns)'] = '(peab olema väiksem kui veergude laius)';
+?>
