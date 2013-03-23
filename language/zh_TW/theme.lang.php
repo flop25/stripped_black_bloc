a78be33fd4415d1f90e02493d6690d2827e4db8a
TranslationTeam <translate-github@piwigo.org>
2013-03-23 21:28:44 +0000

    [stripped_columns] Add zh_TW, thanks to : tedx
    git-svn-id: http://piwigo.org/svn/extensions/stripped_black_bloc@21809 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/zh_TW/theme.lang.php
@@ -0,0 +1,42 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['(it need to be inferior than the width of columns)'] = '（它需要小於欄位的寬度）';
+$lang['(set 0 to disable)'] = '(設定為0來停用)';
+$lang['... starting to the position number'] = '...開始向位置的數字';
+$lang['About big thumbnails, for the albums :'] = '關於大縮略圖給相冊:';
+$lang['About portrait/landscape pictures :'] = '關於肖像/風景圖片(請注意:插件自動管理全景的縮略圖):';
+$lang['Create a big thumbnail every'] = '建立每一個大的縮略圖';
+$lang['Display big thumbnails for landscape pictures'] = '顯示大的景觀圖片的縮略圖';
+$lang['Displaying options'] = '顯示選項';
+$lang['No Maximum Height and the width is the one set above'] = '沒有最大高度和寬度是以上的設定';
+$lang['Page'] = '頁面';
+$lang['Stripped & Columns Theme Configuration'] = 'Stripped & Columns, 設定頁';
+$lang['The parent-theme Stripped need to be enabled in order to use its parameters'] = '主題的剝線必須啟用才能使用它的參數';
+$lang['Use the width parameter above as a maximum heigth'] = '使用以上的寬度參數為最大身度';
+$lang['Width of columns'] = '欄位的寬度';
+$lang['Width of thumbnails'] = '縮略圖的寬度';
+$lang['always generate big thumbnails'] = '產生大的縮略圖';
+$lang['color main'] = '不是灰色的顏色：';
+$lang['don\'t generate big thumbnails'] = '不產生大的縮略圖';
+$lang['use the same config as above'] = '使用相同的設定如上';
+?>
