ca1ce2d57a91e8858ac201a5f658e22bb50a0836
TranslationTeam <translate-github@piwigo.org>
2013-07-04 16:06:05 +0000

    [stripped_columns] Update nl_NL, thanks to : Kees Hessels
    git-svn-id: http://piwigo.org/svn/extensions/stripped_black_bloc@23769 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/nl_NL/theme.lang.php
+++ language/nl_NL/theme.lang.php
@@ -22,13 +22,13 @@
 // +-----------------------------------------------------------------------+
 $lang['(it need to be inferior than the width of columns)'] = '(het moet kleiner zijn dan de breedte van de kolommen)';
 $lang['(set 0 to disable)'] = '(zet op 0 om uit te schakelen)';
-$lang['... starting to the position number'] = '...beginnend bij positie nummer';
+$lang['... starting to the position number'] = '...beginnend bij positienummer';
 $lang['About big thumbnails, for the albums :'] = 'Over grote miniaturen, voor de albums:';
 $lang['Create a big thumbnail every'] = 'Genereer een grote miniatuur elke';
 $lang['Displaying options'] = 'Weergave opties';
 $lang['Page'] = 'Pagina';
 $lang['Stripped & Columns Theme Configuration'] = 'Stripped & Columns, Instellingen-pagina';
-$lang['The parent-theme Stripped need to be enabled in order to use its parameters'] = 'Het hoofd thema Stripped moet actief zijn, om de instellingen van dit thema te kunnen gebruiken';
+$lang['The parent-theme Stripped need to be enabled in order to use its parameters'] = 'Het hoofdthema Stripped moet actief zijn, om de instellingen van dit thema te kunnen gebruiken';
 $lang['Width of columns'] = 'Breedte van kolommen';
 $lang['Width of thumbnails'] = 'Breedte van miniaturen';
 $lang['always generate big thumbnails'] = 'genereer altijd grote miniaturen';
@@ -36,7 +36,12 @@ $lang['color main'] = 'Niet grijze kleur:';
 $lang['don\'t generate big thumbnails'] = 'genereer geen grote miniaturen';
 $lang['use the same config as above'] = 'gebruik dezelfde instellingen als boven';
 $lang['No Maximum Height and the width is the one set above'] = 'Geen maximale hoogte en de breedte is zoals hierboven aangegeven';
-$lang['Display big thumbnails for landscape pictures'] = 'Gebruik grote miniaturen voor landschap-afbeeldingen';
-$lang['About portrait/landscape pictures :'] = 'Over portret/landschap foto\'s (Hou er rekening mee dat deze plugin de miniaturen van panorama\'s automatisch beheert):';
+$lang['Display big thumbnails for landscape pictures'] = 'Gebruik grote miniaturen voor landschapafbeeldingen';
+$lang['About portrait/landscape pictures :'] = 'Over portret-/landschapafbeeldingen (Hou er rekening mee dat deze plugin de miniaturen van panorama\'s automatisch beheert):';
 $lang['Use the width parameter above as a maximum heigth'] = 'Gebruik de breedte-parameter hierboven als maximum hoogte';
+$lang['Effect to use when the mouse is over the thumbnails:'] = 'Welk effect moet gebruikt worden wanneer de muis over een miniatuur beweegt:';
+$lang['For albums, the border becomes white'] = 'Voor albums, de rand wordt wit';
+$lang['No effect'] = 'Geen effect';
+$lang['Opacity at 100%, then 75% on mouseover'] = 'Ondoorzichtigheid 100%, 75% als de muis er over beweegt';
+$lang['Opacity at 75%, then 100% on mouseover'] = 'Ondoorzichtigheid 75%, 100% als de muis er over beweegt';
 ?>
