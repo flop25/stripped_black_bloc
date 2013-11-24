5c3d10cb1e84293c724f725e2ae17442cfc9e889
TranslationTeam <translate-github@piwigo.org>
2013-11-24 07:15:27 +0000

    [stripped_columns] Update eo_EO, thanks to : sylvbarrier
    git-svn-id: http://piwigo.org/svn/extensions/stripped_black_bloc@25666 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/eo_EO/theme.lang.php
@@ -0,0 +1,47 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Stripped & Columns Theme Configuration'] = 'Strioj & Kolumnoj, Agordo-paĝo';
+$lang['The parent-theme Stripped need to be enabled in order to use its parameters'] = 'La parenca temo Strioj estu aktivigita por uzi ĝiaj parametroj';
+$lang['always generate big thumbnails'] = 'ĉiam generi grandajn miniaturojn';
+$lang['color main'] = 'Negriza koloro:';
+$lang['don\'t generate big thumbnails'] = 'ne generi grandajn miniaturojn';
+$lang['use the same config as above'] = 'uzi la saman agordon kiel ĉisupre';
+$lang['(it need to be inferior than the width of columns)'] = '(estu nepre strikte malpli granda ol la kolumna larĝeco)';
+$lang['(set 0 to disable)'] = '(meti o por malaktivigi)';
+$lang['... starting to the position number'] = '... komencante de la pozicio numero';
+$lang['About big thumbnails, for the albums :'] = 'Pri grandaj miniaturoj, por la fotaroj:';
+$lang['About portrait/landscape pictures :'] = 'Pri verikalaj/horizontalaj bildoj (Notu ke la kromaĵo memadministras la miniaturojn de panoramoj):';
+$lang['Create a big thumbnail every'] = 'Krei granda miniaturo por ĉiuj';
+$lang['Display big thumbnails for landscape pictures'] = 'Vidigi grandajn miniaturojn por horizontalaj bildoj';
+$lang['Displaying options'] = 'Vidigi opcioj';
+$lang['Effect to use when the mouse is over the thumbnails:'] = 'Efekto uzenda kiam muso superpasas la miniaturojn:';
+$lang['For albums, the border becomes white'] = 'Por fotaroj, la bordero iĝas blanka';
+$lang['No Maximum Height and the width is the one set above'] = 'Neniu maksima alteco kaj la larĝeco estas la valoro ĉisupre agordita';
+$lang['No effect'] = 'Neniu efekto';
+$lang['Opacity at 100%, then 75% on mouseover'] = 'Opakeco je 100%, kaj je 75% dum musumo';
+$lang['Opacity at 75%, then 100% on mouseover'] = 'Opakeco je 75%, kaj je 100% dum musumo';
+$lang['Page'] = 'Paĝo';
+$lang['Use the width parameter above as a maximum heigth'] = 'Uzi la ĉisupran larĝeco-parametron kiel maksimuma alteco';
+$lang['Width of columns'] = 'Kolumna larĝeco';
+$lang['Width of thumbnails'] = 'Miniatura larĝeco';
+?>
