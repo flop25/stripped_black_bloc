2e9857b62410eb7d66a0e27d2784bbcc1f0e41be
TranslationTeam <translate-github@piwigo.org>
2013-12-27 14:54:51 +0000

    [stripped_columns] Update sl_SI, thanks to : simon.krajnc
    git-svn-id: http://piwigo.org/svn/extensions/stripped_black_bloc@26267 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/sl_SI/theme.lang.php
+++ language/sl_SI/theme.lang.php
@@ -23,4 +23,25 @@
 $lang['Displaying options'] = 'Zaslonske možnosti';
 $lang['No effect'] = 'Brez učinka';
 $lang['Page'] = 'Stran';
+$lang['use the same config as above'] = 'uporabi enako nastavitev';
+$lang['No Maximum Height and the width is the one set above'] = 'Ni določene max višine in širine';
+$lang['Use the width parameter above as a maximum heigth'] = 'Uporabi parametre za max. višino';
+$lang['Width of columns'] = 'Širina stolpcev';
+$lang['Width of thumbnails'] = 'Širina thumbnail sličic';
+$lang['always generate big thumbnails'] = 'vedno generiraj velike thumbnail sličice';
+$lang['color main'] = 'Ni sive barve:';
+$lang['don\'t generate big thumbnails'] = 'ne generiraj thumbnail sličic';
+$lang['Effect to use when the mouse is over the thumbnails:'] = 'Izbran učinek za prehod miške preko thumbnail sličice';
+$lang['For albums, the border becomes white'] = 'Za albume, okvir postane bel';
+$lang['Opacity at 100%, then 75% on mouseover'] = 'Prosojnost 100%, nato 75% ob prehodu miške';
+$lang['Opacity at 75%, then 100% on mouseover'] = 'Prosojnost 75%, nato 100% ob prehodu miške';
+$lang['Stripped & Columns Theme Configuration'] = 'Razdeljeni & stolpci, Konfiguracijska stran';
+$lang['The parent-theme Stripped need to be enabled in order to use its parameters'] = 'Starševska tema Stripped mora biti omogočena za uporabo teh parametrov';
+$lang['(it need to be inferior than the width of columns)'] = '(mora biti manjši kot širina stolpca)';
+$lang['About portrait/landscape pictures :'] = 'O portretnih/pokrajinskih fotografijah (Vtičnik avtomatsko upravlja thumbnail sličice panoram):';
+$lang['Display big thumbnails for landscape pictures'] = 'Prikaži velike thumbnail sličice za pokrajinske fotografije';
+$lang['Create a big thumbnail every'] = 'Naredi velike thumbnail sličice vsakič';
+$lang['... starting to the position number'] = '...začetek postavitve števila';
+$lang['About big thumbnails, for the albums :'] = 'Glede velikih thumbnail sličic, za albume:';
+$lang['(set 0 to disable)'] = '(označi za razveljavitev)';
 ?>
