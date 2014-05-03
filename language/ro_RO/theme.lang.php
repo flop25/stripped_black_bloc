3ee5e661555389019351ebc1457001fe17bf7de1
TranslationTeam <translate-github@piwigo.org>
2014-05-03 13:49:34 +0000

    [stripped_columns] Add ro_RO, thanks to : cheilenereinfo
    git-svn-id: http://piwigo.org/svn/extensions/stripped_black_bloc@28328 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/ro_RO/theme.lang.php
@@ -0,0 +1,41 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['use the same config as above'] = 'foloseste aceeasi configurare ca mai sus';
+$lang['don\'t generate big thumbnails'] = 'nu genera pictograme mari';
+$lang['color main'] = 'Fara culoarea gri';
+$lang['always generate big thumbnails'] = 'intodeauna generaza pictogramele mari';
+$lang['Width of thumbnails'] = 'Latimea pictogramelor';
+$lang['Width of columns'] = 'Latimea coloanelor';
+$lang['For albums, the border becomes white'] = 'La albume, mraginea devine alba';
+$lang['Opacity at 75%, then 100% on mouseover'] = 'Opacitatea la 75% si 100% cand ursorul este deasupra';
+$lang['Opacity at 100%, then 75% on mouseover'] = 'Opacitatea la 100% si 75% cand ursorul este deasupra';
+$lang['No effect'] = 'Fara efect';
+$lang['Page'] = 'Pagina';
+$lang['Effect to use when the mouse is over the thumbnails:'] = 'Effect folosit cand cursorul este deasupra pictogramelor';
+$lang['Displaying options'] = 'Afiseaza optiuni';
+$lang['Display big thumbnails for landscape pictures'] = 'Afiseaza pictogramele mari pentru pozele landscape';
+$lang['Create a big thumbnail every'] = 'Creeaza o pictograma mare la fiecare';
+$lang['About big thumbnails, for the albums :'] = 'Despre pictogramele mari, pentru albume';
+$lang['... starting to the position number'] = '...incepand cu numarul de pozitie';
+$lang['(set 0 to disable)'] = '(seteaza 0 pentru a dezactiva)';
+$lang['(it need to be inferior than the width of columns)'] = '(e nevoie sa fie strict mai mica decat latimea coloanelor)';
